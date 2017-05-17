1349698f2d7387f183869604e177d082c8c564bf
DevonMars <devon_marsham_16@live.nl>
2017-05-18 01:07:44 +0200

    config file working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http    =require('http');
 var express =require('express');
 
 var app = express();
-app.set('PORT')
+app.set('PORT', config.webPort);
 app.all('*', function (req, res, next) {
     console.log(req.method + " " + req.url);
     next();
@@ -16,8 +16,11 @@ app.get('/api/v1/hello', function (req, res, next) {
     res.contentType('application/json');
     res.json({"msg":"Hello to you all!! Have a nice NodeJS day"});
 });
-
-app.listen(8080,function () {
-    console.log('The magic happens at http://localhost:8080');
-
+var port = process.env.PORT || app.get('PORT');
+app.listen(port, function () {
+    console.log('The magic happens at http://localhost:' + port);
 });
+// app.listen(8080,function () {
+//     console.log('The magic happens at http://localhost:8080');
+
+// });
